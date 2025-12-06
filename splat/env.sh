a2c725f9e524acb8cdc1baf8d7fd15640233030c
1e100 <38598618+1e100@users.noreply.github.com>
2025-12-06 12:14:20 -0800

    A working version of splat/env.sh

--- splat/env.sh
+++ splat/env.sh
@@ -4,13 +4,24 @@ eval "$(conda shell.bash hook)"
 
 conda create --name drawer_splat -y python=3.8
 conda activate drawer_splat
-conda install -y nvidia::cuda=11.8.0
+conda config --env --set channel_priority strict
+
+conda install -y \
+  -c "nvidia/label/cuda-11.8.0" \
+  cuda-toolkit=11.8.0 \
+  cuda-nvcc=11.8.89 \
+  cuda-runtime=11.8.0
 
 export CUDA_HOME="$CONDA_PREFIX"
 export PATH="$CUDA_HOME/bin:$PATH"
 export LD_LIBRARY_PATH="$CUDA_HOME/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH"
+export CC=/usr/bin/gcc-11
+export CXX=/usr/bin/g++-11
+export CUDA_ARCH_LIST="8.6"
+export TORCH_CUDA_ARCH_LIST="${CUDA_ARCH_LIST}"
+export CMAKE_CUDA_ARCHITECTURES="$(echo "${CUDA_ARCH_LIST}" | tr ';' ' ' | tr -d '.')"
 
-pip install torch==2.1.2+cu118 torchvision==0.16.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.1.2+cu118 torchvision==0.16.2+cu118 matplotlib --extra-index-url https://download.pytorch.org/whl/cu118
 pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 
 pip install -e .
